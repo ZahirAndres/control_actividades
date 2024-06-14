aa66d71c487c3a60703983fcba48c18ba761a351
ZahirAndres <113659010+ZahirAndres@users.noreply.github.com>
2024-06-14 12:35:07 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReservacionesComponent } from './components/reservaciones/reservaciones.component';
+import { ContratoComponent } from './components/contrato/contrato.component';
+import { ReservanosComponent } from './components/reservanos/reservanos.component';
+const routes: Routes = [
+  {
+    path : '',
+    redirectTo : '/reservaciones' ,
+    pathMatch : 'full'
+  },
+  {
+    path : 'reservaciones',
+    component :  ReservacionesComponent
+  },
+  {
+    path : 'contratos',
+    component :  ContratoComponent
+    
+  },
+  {
+    path : 'reservanos',
+    component :  ReservanosComponent
+    
+  }
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { ReservacionesComponent } from './components/reservaciones/reservaciones.component';
+import { ReglamentoComponent } from './components/reglamento/reglamento.component';
+import { IngresarComponent } from './components/ingresar/ingresar.component';
+import { RecuperarContrasComponent } from './components/recuperar-contras/recuperar-contras.component';
+import { NuevoUsuarioComponent } from './components/nuevo-usuario/nuevo-usuario.component';
+import { SuguerenciasQuejasComponent } from './components/suguerencias-quejas/suguerencias-quejas.component';
+import { OficiosReservasComponent } from './components/oficios-reservas/oficios-reservas.component';
+import { AdministrarReservasComponent } from './components/administrar-reservas/administrar-reservas.component';
+import { ContratoComponent } from './components/contrato/contrato.component';
+import { ReservanosComponent } from './components/reservanos/reservanos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    ReservacionesComponent,
+    ReglamentoComponent,
+    IngresarComponent,
+    RecuperarContrasComponent,
+    NuevoUsuarioComponent,
+    SuguerenciasQuejasComponent,
+    OficiosReservasComponent,
+    AdministrarReservasComponent,
+    ContratoComponent,
+    ReservanosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
